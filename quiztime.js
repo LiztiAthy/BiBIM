d30783c02d818f2e45171fb9d2863a159069fc79
LiztiAthy <nrlizzati29@gmail.com>
2024-06-18 20:21:35 +0800

    Update quiztime.js

--- quiztime.js
+++ quiztime.js
@@ -1,9 +1,11 @@
 const cards = document.querySelectorAll(".card"),
-timeTag = document.querySelector(".time b"),
-flipsTag = document.querySelector(".flips b"),
-refreshBtn = document.querySelector(".details button");
+    timeTag = document.querySelector(".time b"),
+    flipsTag = document.querySelector(".flips b"),
+    refreshBtn = document.querySelector(".details button"),
+    celebrationOverlay = document.getElementById("celebration-overlay"),
+    refreshButton = document.getElementById("refresh-button");
 
-let maxTime = 20;
+let maxTime = 30;
 let timeLeft = maxTime;
 let flips = 0;
 let matchedCard = 0;
@@ -12,38 +14,42 @@ let isPlaying = false;
 let cardOne, cardTwo, timer;
 
 function initTimer() {
-    if(timeLeft <= 0) {
+    if (timeLeft <= 0) {
         return clearInterval(timer);
     }
     timeLeft--;
     timeTag.innerText = timeLeft;
 }
 
-function flipCard({target: clickedCard}) {
-    if(!isPlaying) {
+function flipCard({ target: clickedCard }) {
+    if (!isPlaying) {
         isPlaying = true;
         timer = setInterval(initTimer, 1000);
     }
-    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+    if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
         flips++;
         flipsTag.innerText = flips;
         clickedCard.classList.add("flip");
-        if(!cardOne) {
+        if (!cardOne) {
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
         disableDeck = true;
         let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+            cardTwoImg = cardTwo.querySelector(".back-view img").src;
         matchCards(cardOneImg, cardTwoImg);
     }
 }
 
 function matchCards(img1, img2) {
-    if(img1 === img2) {
+    if (img1 === img2) {
         matchedCard++;
-        if(matchedCard == 6 && timeLeft > 0) {
-            return clearInterval(timer);
+        if (matchedCard == 6 && timeLeft > 0) {
+            clearInterval(timer);
+            setTimeout(() => {
+                celebrationOverlay.classList.add("show");
+            }, 1000);
+            return;
         }
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
@@ -72,6 +78,7 @@ function shuffleCard() {
     timeTag.innerText = timeLeft;
     flipsTag.innerText = flips;
     disableDeck = isPlaying = false;
+    celebrationOverlay.classList.remove("show");
 
     let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
@@ -89,6 +96,7 @@ function shuffleCard() {
 shuffleCard();
 
 refreshBtn.addEventListener("click", shuffleCard);
+refreshButton.addEventListener("click", shuffleCard);
 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
